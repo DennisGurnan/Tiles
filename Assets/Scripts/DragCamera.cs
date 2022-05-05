ea4190f98396e0843a198509d73075706cda5c84
Dennis Gurman <dennis.gurman@gmail.com>
2022-05-05 10:10:31 +0700

    Cosmetics

--- Assets/Scripts/DragCamera.cs
+++ Assets/Scripts/DragCamera.cs
@@ -42,13 +42,13 @@ public class DragCamera : MonoBehaviour
         if (cameraDragging)
         {
 
-            if (Input.GetMouseButtonDown(1))
+            if (Input.GetMouseButtonDown(0))
             {
                 dragOrigin = Input.mousePosition;
                 return;
             }
 
-            if (!Input.GetMouseButton(1)) return;
+            if (!Input.GetMouseButton(0)) return;
 
             Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
             Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
